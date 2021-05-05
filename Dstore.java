324eae3df17348712f54e032fa2fc0c053bfe5fd
alek_martir <alekmartir@gmail.com>
2021-05-05 18:46:13 +0100

    Feat: improved logging

--- Dstore.java
+++ Dstore.java
@@ -5,29 +5,53 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class Dstore {
 
+	private static final String DATE_FORMAT = "yyyy/MM/dd HH:mm:ss";
 	private static int port;
 	private static int cport;
 	private static int timeout;
 
 	private static PrintWriter pWriter;
 	private static BufferedReader reader;
+	private static Socket socket;
 
 	private static File file_folder;
 
 	private static void printMes(String mes) {
-		System.out.println("[DSTORE]: " + mes);
+		String timeStamp = new SimpleDateFormat(DATE_FORMAT).format(Calendar.getInstance().getTime());
+		System.out.println("[DSTORE: " + timeStamp + "]: " + mes);
 	}
 
 	private static void printErr(String mes) {
-		System.err.println("[DSTORE]: " + mes);
+		String timeStamp = new SimpleDateFormat(DATE_FORMAT).format(Calendar.getInstance().getTime());
+		System.err.println("[DSTORE: " + timeStamp + "]: " + mes);
+	}
+
+	private static String stampMes(String mes) {
+		String timeStamp = new SimpleDateFormat(DATE_FORMAT).format(Calendar.getInstance().getTime());
+		return (timeStamp + ": " + mes);
 	}
 
 	public static void main(String[] args) {
 		initArgs(args);
+		initLogger();
 
+		try {
+			initSocket();
+			joinController();
+			// socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	private static void initLogger() {
 		try {
 			DstoreLogger.init(Logger.LoggingType.ON_FILE_AND_TERMINAL, port);
 		} catch (IOException e) {
@@ -35,22 +59,40 @@ public class Dstore {
 			e.printStackTrace();
 			System.exit(1);
 		}
+	}
+
+	private static void initSocket() throws IOException {
+		printMes("Establishing connection with Controller on port " + cport + ", local address");
+		InetAddress address = InetAddress.getLocalHost();
+
+		pWriter = new PrintWriter(socket.getOutputStream(), true);
+		reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		socket = new Socket(address, cport, address, port);
+		socket.setSoTimeout(timeout);
+
+		printMes("Succesfully established connection! Local port: " + port);
+	}
+
+	// TODO: Might just ask to send the response as LIST
+	private static void joinController() throws IOException {
+		printMes("Attempting to join the controller");
+		String message = Protocol.JOIN_TOKEN + " " + port;
+		pWriter.println(message);
+		DstoreLogger.getInstance().messageSent(socket, stampMes(message));
+
+		String response;
 
 		try {
-			printMes("Establishing connection with Controller on port " + cport + ", local address");
-			InetAddress address = InetAddress.getLocalHost();
-			Socket connection = new Socket(address, cport, address, port);
-
-			printMes("Succesfully established connection! Local port: " + port);
-
-			pWriter = new PrintWriter(connection.getOutputStream(), true);
-			interactiveCommands();
-
-			connection.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(1);
+			response = reader.readLine();
+			if (response == null) {
+				printErr("Connection Lost");
+			} else {
+				printMes("Server: " + response);
+			}
+		} catch (SocketTimeoutException e) {
+			printMes("Joined Successfully!");
 		}
+
 	}
 
 	private static void interactiveCommands() throws IOException {
@@ -63,6 +105,11 @@ public class Dstore {
 		while ((mes = reader.readLine()) != "quit") {
 			pWriter.println(mes);
 			System.out.print("> ");
+
+			if (reader.readLine() == Protocol.ERROR_NOT_ENOUGH_DSTORES_TOKEN) {
+				printErr("Not enough dstores joined yet, try again later!");
+				break;
+			}
 		}
 
 	}
